b9faa36f7c6157659c0582b5aa9d48bd5ceba537
Lorenzo Giacomel <lorenzo.giacomel@cern.ch>
2019-02-22 15:00:24 +0100

    added lbl

---
+++ lbl/000_run_simulation.py
@@ -0,0 +1,11 @@
+import sys, os
+BIN = os.path.expanduser("../../")
+sys.path.append(BIN)
+
+
+from PyECLOUD.buildup_simulation import BuildupSimulation
+
+# Build the simulation object
+sim = BuildupSimulation(extract_sey=False)
+# Run the simulation 
+sim.run()
